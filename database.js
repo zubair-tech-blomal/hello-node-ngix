0baa1dfeb849d6f88cf5dd131fb1d54c1a7e018b
zubair-tech-blomal <zubair@blomal.com>
2024-09-16 17:11:30 +0300

    updated

---
+++ database.js
@@ -0,0 +1,22 @@
+const mysql = require('mysql2');
+require('dotenv').config();
+
+// Create a connection to the database
+const connection = mysql.createConnection({
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  user: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE
+});
+
+// Connect to the database
+connection.connect((err) => {
+  if (err) {
+    console.error('Error connecting to the database:', err.stack);
+    return;
+  }
+  console.log('Connected to the database as ID ' + connection.threadId);
+});
+
+module.exports = connection;
--- server.js
+++ server.js
@@ -1,10 +1,17 @@
-// app.js
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 8080;
+const db = require('./database'); // Import the database connection
 
 app.get('/', (req, res) => {
-  res.send('Hello, Node.js with Nginx!');
+  db.query('SELECT 1 + 1 AS solution', (error, results) => {
+    if (error) {
+      console.error('Database query error:', error);
+      res.status(500).send('Database query error');
+      return;
+    }
+    res.send('The solution is: ' + results[0].solution);
+  });
 });
 
 app.listen(port, () => {
